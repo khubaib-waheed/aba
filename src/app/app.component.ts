bdbfa6171a4bb1b9e138cdcf071b297330e8ed74
khubaib-waheed <khubaibwaheed151@gmail.com>
2025-02-28 17:44:36 +0500

    scroll functionaliy added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, NgZone, PLATFORM_ID, ViewChild } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { interval, timeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   landingPage: boolean = true;
+  page = 'landing'
+  headerColor = 'default-header';
 
-  constructor(private ngZone: NgZone) {}
+  activeMenu: string = 'profile'; // Default active menu
+
+  setActiveMenu(menu: string) {
+    this.activeMenu = menu;
+    this.onScrollToTop()
+  }
+
+  
+
+  constructor(private ngZone: NgZone, @Inject(PLATFORM_ID) private platformId: Object) {}
   date: any;
   now: any;
   targetDate: any = new Date(2025, 5, 20);
@@ -42,6 +53,14 @@ export class AppComponent {
   @ViewChild('minutes', { static: true }) minutes!: ElementRef;
   @ViewChild('seconds', { static: true }) seconds!: ElementRef;
 
+
+  onScrollToTop(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+
+
   ngAfterViewInit() {
     // this.ngZone.runOutsideAngular(() => setInterval(() => {
     //   this.tickTock();
@@ -49,6 +68,14 @@ export class AppComponent {
     //   this.difference = this.difference / (1000 * 60 * 60 * 24);
     //   this.days.nativeElement.innerText = `${Math.floor(this.difference)}d`
     // }, 1000));
+  }
+
+  changePage(activePage: any) {
+    this.page = activePage;
+    this.headerColor = (this.page === 'landing') ? 'default-header' : 'landing-header';
+    if(this.page === 'landing') {
+      window.location.reload()
+    }
   }
 
   tickTock() {
