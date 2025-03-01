a3e5667a6dba21a536f56dfdae6cbc3542fe9675
khubaib-waheed <khubaibwaheed151@gmail.com>
2025-03-01 14:20:36 +0500

    styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, Inject, Input, NgZone, PLATFORM_ID, QueryList, ViewChild, ViewChildren, viewChildren } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, HostListener, Inject, Input, NgZone, OnInit, PLATFORM_ID, QueryList, ViewChild, ViewChildren, viewChildren } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgOtpInputModule } from 'ng-otp-input';
 
 @Component({
   selector: 'app-custom-dropdown',
@@ -21,7 +22,7 @@ import { RouterModule } from '@angular/router';
 })
 export class CustomDropdownComponent {
   isOpen = false;
-  selectedOption = 'En';
+  @Input() selectedOption = 'En';
   @Input() options: any = [];
 
   toggleDropdown() {
@@ -60,21 +61,23 @@ export class CustomDropdownComponent {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, CommonModule, CustomDropdownComponent],
+  imports: [RouterModule, CommonModule, CustomDropdownComponent, NgOtpInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   landingPage: boolean = true;
-  page = 'landing'
+  page = 'sign-in'
   headerColor = 'default-header';
 
   activeMenu: string = 'profile'; // Default active menu
 
   title = 'aba-without-ssr';
+  loggedIn: boolean = false;
 
   isPasswordVisible = false;
+  isConfirmPasswordVisible = false;
 
   imagePreviews: string[] = Array(7).fill(''); // Holds image previews for 7 slots
 
@@ -84,11 +87,7 @@ export class AppComponent {
     this.fileInputs.get(index)?.nativeElement.click();
   }
 
-  
 
-  
-
-  constructor(private ngZone: NgZone, @Inject(PLATFORM_ID) private platformId: Object) {}
   date: any;
   now: any;
   targetDate: any = new Date(2025, 5, 20);
@@ -111,17 +110,88 @@ export class AppComponent {
   currentTime: any = `${
     this.months[this.targetDate.getMonth()]
   } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
+  
 
   @ViewChild('days', { static: true }) days!: ElementRef;
   @ViewChild('hours', { static: true }) hours!: ElementRef;
   @ViewChild('minutes', { static: true }) minutes!: ElementRef;
   @ViewChild('seconds', { static: true }) seconds!: ElementRef;
 
+  paramValue: any = '';
+
+  otp: any;
+
+  config = {
+    allowNumbersOnly: false,
+    length: 4,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '50px',
+      'height': '50px'
+    }
+  };
+
+  constructor(
+    private ngZone: NgZone, 
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private route: ActivatedRoute
+  ) {}
+  
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.paramValue = params['key']; // Get query param value
+      if(this.paramValue === 'home') {
+        this.loggedIn = true;
+        this.page = 'landing';
+      }
+
+      if(this.paramValue === 'signup') {
+        this.page = 'signup';
+      }
+    });
+  }
 
   onScrollToTop(): void {
     if (isPlatformBrowser(this.platformId)) {
       window.scrollTo({ top: 0, behavior: 'smooth' });
     }
+  }
+
+  login() {
+    if (isPlatformBrowser(this.platformId)) {
+      const params = new URLSearchParams(window.location.search);
+      params.set('key', 'home');  // Add or update a query parameter
+
+    window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
+      window.location.reload();
+    }
+  }
+
+  goToSignInPage() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.history.replaceState({}, '', window.location.pathname);
+      window.location.reload();
+    }
+  }
+
+  signup() {
+    if (isPlatformBrowser(this.platformId)) {
+      const params = new URLSearchParams(window.location.search);
+      params.set('key', 'signup');  // Add or update a query parameter
+
+    window.history.replaceState({}, '', `${window.location.pathname}?${params.toString()}`);
+      window.location.reload();
+    }
+  }
+
+  enterEmail() {
+
+  }
+
+  onOtpChange(otp: any) {
+    this.otp = otp;
   }
 
 
@@ -158,8 +228,14 @@ export class AppComponent {
     }
   }
 
-  togglePassword(): void {
-    this.isPasswordVisible = !this.isPasswordVisible;
+  togglePassword(type: any): void {
+    if(type === 'password') {
+      this.isPasswordVisible = !this.isPasswordVisible;
+    }
+    else {
+      this.isConfirmPasswordVisible = !this.isConfirmPasswordVisible;
+    }
+    
   }
 
   tickTock() {
