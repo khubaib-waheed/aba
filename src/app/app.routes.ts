7eeecad5d684ad15fe40894a748619767582a407
khubaib-waheed <khubaibwaheed151@gmail.com>
2025-03-05 13:41:53 +0500

    routes cleared

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './pages/auth/auth.component';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-export const routes: Routes = [
-    // {
-    //     path: '',
-    //     component: HomeComponent, // Routes with header & footer
-    //     children: [
-    //       { path: '', redirectTo: 'home', pathMatch: 'full' },
-    //       { path: 'home', component: LandingPageComponent },
-    //       { path: 'contact', component: ContactComponent }
-    //     ]
-    // },
-    // {
-    //     path: '',
-    //     component: AuthComponent, // Routes without header & footer
-    //     children: [
-    //       { path: 'sign-in', component: SignInComponent },
-    //     ]
-    // },
-    // { path: '', redirectTo: 'home', pathMatch: 'full' },
-    // { path: '**', redirectTo: 'home' } // Handle unknown routes
-];
+export const routes: Routes = [];
